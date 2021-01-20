ed09c6078a25427891346d5961048b9e7e8294e0
rqguzman <rafael.guzman@outlook.com>
2021-01-20 16:48:10 -0300

    :sparkles:feat(PagesController): Add specific page retrieval

--- src/main/java/com/guzmanr/cmsrestclient/controlers/PagesController.java
+++ src/main/java/com/guzmanr/cmsrestclient/controlers/PagesController.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
@@ -20,6 +23,19 @@ public class PagesController {
 
         Page page = rest.getForObject("http://localhost:8080/pages/home", Page.class);
         model.addAttribute("page", page);
+        return "page";
+    }
+
+    @GetMapping("/{slug}")
+    public String page(@PathVariable String slug, Model model){
+
+        try {
+
+            Page page = rest.getForObject("http://localhost:8080/pages/{slug}", Page.class, slug);
+            model.addAttribute("page", page);
+        } catch (final HttpClientErrorException e) {
+            return "redirect:/";
+        }
         return "page";
     }
 }
