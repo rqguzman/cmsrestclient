c9d87a8eafcc5a1b78f79b559335a1657dd14d63
rqguzman <rafael.guzman@outlook.com>
2021-01-20 15:18:29 -0300

    :tada: Project created

---
+++ src/main/java/com/guzmanr/cmsrestclient/CmsrestclientApplication.java
@@ -0,0 +1,13 @@
+package com.guzmanr.cmsrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsrestclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsrestclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guzmanr/cmsrestclient/CmsrestclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guzmanr.cmsrestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsrestclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
